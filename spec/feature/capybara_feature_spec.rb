07e257817880d43255a082208251b7abbd46f492
Georgia Mills <georgee_123@hotmail.com>
2016-07-19 12:13:11 +0100

    first user story

---
+++ spec/feature/capybara_feature_spec.rb
@@ -0,0 +1,13 @@
+#require 'capybara'
+require '../Battle/app'
+require 'spec_helper'
+
+feature 'Enter names' do
+  scenario 'submitting names' do
+    visit('/')
+    fill_in :player_1_name, with: 'Thady'
+    fill_in :player_2_name, with: 'Georgia'
+    click_button 'Submit'
+    expect(page).to have_content 'Thady vs. Georgia'
+end
+end
